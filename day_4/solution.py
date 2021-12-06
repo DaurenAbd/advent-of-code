445a871de48d807d3d578b078fdcaa04e4048df8
Dauren Abdykaparov <daurenabd@gmail.com>
2021-12-06 00:39:27 +0000

    rewrite day-4

--- day_4/solution.py
+++ day_4/solution.py
@@ -1,51 +1,58 @@
 import math
+from dataclasses import dataclass
 from typing import Dict, List, Tuple
 
-Board = List[List[int]]
+
+@dataclass(order=True)
+class Ball:
+    time: int
+    num: int
 
 
-def parse_board(lines: List[str]) -> Board:
-    return [[int(num) for num in line.split()] for line in lines]
+class Board:
+    def __init__(self, lines: List[str]):
+        self.board = [[int(num) for num in line.split()] for line in lines]
+
+    def as_timing_board(self, ball_times: Dict[int, int]) -> List[List[Ball]]:
+        return [[Ball(ball_times.get(num, math.inf), num) for num in row] for row in self.board]
+
+    def winning_time_and_score(self, ball_times: Dict[int, int]) -> Tuple[int, int]:
+        timing_board = self.as_timing_board(ball_times)
+        rows = min(max(timing_board[i]) for i in range(5))
+        cols = min(max(timing_board[i][j] for i in range(5)) for j in range(5))
+        winning_ball = min(rows, cols)
+
+        unmarked = sum(ball.num for row in timing_board for ball in row if ball.time > winning_ball.time)
+
+        return winning_ball.time, unmarked * winning_ball.num
 
 
-def read_input() -> Tuple[List[int], List[Board]]:
+def read_input() -> Tuple[List[Ball], List[Board]]:
     with open('input.txt', 'r') as reader:
         raw_lines = reader.readlines()
         stripped_lines = map(lambda line: line.strip(), raw_lines)
         non_empty_lines = filter(lambda line: line, stripped_lines)
         lines = list(non_empty_lines)
 
-        moves = [int(move) for move in lines[0].split(',')]
-        boards = [parse_board(lines[i - 5: i]) for i in range(6, len(lines) + 1, 5)]
+        balls = [Ball(t, int(num)) for t, num in enumerate(lines[0].split(','))]
+        boards = [Board(lines[i - 5: i]) for i in range(6, len(lines) + 1, 5)]
 
-        return moves, boards
-
-
-def winning_time_and_score(board: Board, move_times: Dict[int, int]) -> Tuple[int, int]:
-    timing_board = [[(move_times.get(num, math.inf), num) for num in row] for row in board]
-
-    rows = min(max(timing_board[i]) for i in range(5))
-    cols = min(max(timing_board[i][j] for i in range(5)) for j in range(5))
-    winning_time, last_move = min(rows, cols)
-
-    unmarked = sum(num for row in timing_board for t, num in row if t > winning_time)
-
-    return winning_time, unmarked * last_move
+        return balls, boards
 
 
-def task1(moves: List[int], boards: List[Board]) -> int:
-    move_times = {move: i for i, move in enumerate(moves)}
-    results = [winning_time_and_score(board, move_times) for board in boards]
+def task1(balls: List[Ball], boards: List[Board]) -> int:
+    ball_times = {ball.num: ball.time for ball in balls}
+    results = [board.winning_time_and_score(ball_times) for board in boards]
     return min(results)[1]
 
 
-def task2(moves: List[int], boards: List[Board]) -> int:
-    move_times = {move: i for i, move in enumerate(moves)}
-    results = [winning_time_and_score(board, move_times) for board in boards]
+def task2(balls: List[Ball], boards: List[Board]) -> int:
+    ball_times = {ball.num: ball.time for ball in balls}
+    results = [board.winning_time_and_score(ball_times) for board in boards]
     return max(results)[1]
 
 
 if __name__ == '__main__':
-    moves, boards = read_input()
-    print(task1(moves, boards))
-    print(task2(moves, boards))
+    balls, boards = read_input()
+    print(task1(balls, boards))
+    print(task2(balls, boards))
