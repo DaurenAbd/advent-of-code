be5965197bb18369e09af179bccd431e35e7a646
Dauren Abdykaparov <daurenabd@gmail.com>
2021-12-14 03:41:16 +0600

    day-13 OCR

--- 2021/day_13/solution.py
+++ 2021/day_13/solution.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from typing import List, Set, Tuple
 
+from PIL import Image
+import pytesseract
+
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 
 @dataclass(eq=True, frozen=True)
 class Point:
@@ -20,31 +26,43 @@ class Matrix:
     def __init__(self, points: Set[Point]):
         self.points = points
 
+    def fold_x(self, x: int):
+        right_side = {point for point in self.points if point.x > x}
+        self.points.difference_update(right_side)
+
+        folded_right_side = {Point(2 * x - point.x, point.y) for point in right_side}
+        self.points.update(folded_right_side)
+
+    def fold_y(self, y: int):
+        down_side = {point for point in self.points if point.y > y}
+        self.points.difference_update(down_side)
+
+        folded_down_side = {Point(point.x, 2 * y - point.y) for point in down_side}
+        self.points.update(folded_down_side)
+
     def apply(self, fold: Fold):
         if fold.axis == 'x':
-            on_axis = {point for point in self.points if point.x == fold.value}
-            down_side = {point for point in self.points if point.x > fold.value}
-            self.points.difference_update(on_axis)
-            self.points.difference_update(down_side)
-            folded_down_side = {Point(2 * fold.value - point.x, point.y) for point in down_side}
-            self.points.update(folded_down_side)
+            self.fold_x(fold.value)
         if fold.axis == 'y':
-            on_axis = {point for point in self.points if point.y == fold.value}
-            down_side = {point for point in self.points if point.y > fold.value}
-            self.points.difference_update(on_axis)
-            self.points.difference_update(down_side)
-            folded_down_side = {Point(point.x, 2 * fold.value - point.y) for point in down_side}
-            self.points.update(folded_down_side)
+            self.fold_y(fold.value)
 
-    def __str__(self) -> str:
+    def as_text(self) -> str:
         min_x = min(point.x for point in self.points)
         min_y = min(point.y for point in self.points)
         max_x = max(point.x for point in self.points)
         max_y = max(point.y for point in self.points)
-        table = [['.'] * (max_x - min_x + 1) for _ in range(max_y - min_y + 1)]
+
+        # extra +2's to add 1 pixel padding to the image
+        width = max_x - min_x + 3
+        height = max_y - min_y + 3
+        image = Image.new('L', (width, height), 255)
+
         for point in self.points:
-            table[point.y - min_y][point.x - min_x] = '@'
-        return '\n'.join(''.join(row) for row in table)
+            # extra +1's to add 1 pixel padding to the image
+            image.putpixel((point.x - min_x + 1, point.y - min_y + 1), 0)
+
+        # "psm 8" means that the image will be interpreted a single word
+        return pytesseract.image_to_string(image, config='--psm 8')
 
 
 def read_input() -> Tuple[Set[Point], List[Fold]]:
@@ -78,7 +96,7 @@ def task2(points: Set[Point], folds: List[Fold]) -> str:
     matrix = Matrix(points)
     for fold in folds:
         matrix.apply(fold)
-    return str(matrix)
+    return matrix.as_text()
 
 
 if __name__ == '__main__':
